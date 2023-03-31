188237672a0900cbc74dc68f87f5a302fe2eac87
linazhukov <lina@wolfcola.org>
2023-03-31 18:47:21 -0400

    load routes.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 
 const express = require('express');
+const routes = require('./routes')
 
 module.exports = function(){
     try{
         const app = express();
+        routes(app);
         return app;
     }catch (e){
         console.error(e);
