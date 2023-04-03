329658cac3876be3669cf747a278069a3bda0948
linazhukov <lina@wolfcola.org>
2023-04-02 20:47:39 -0400

    add redis config

--- config/default.js
+++ config/default.js
@@ -1,7 +1,9 @@
 const pg = require('./pg-config');
+const redis = require('./redis-config');
 
 module.exports = {
     PORT: process.env.port || 3000,
     HOST: process.env.host || 'localhost',
     pg,
+    redis
 }
